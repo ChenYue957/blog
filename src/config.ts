989953255cee32862fef7d6c951005274c843f76
尘钥ChenYue <jrxiao957@gmail.com>
2026-02-17 13:49:50 +0800

    Initial commit: Fuwari blog setup

---
+++ src/config.ts
@@ -0,0 +1,111 @@
+import type {
+	ImageFallbackConfig,
+	LicenseConfig,
+	NavBarConfig,
+	ProfileConfig,
+	SiteConfig,
+	UmamiConfig,
+} from "./types/config";
+import { LinkPreset } from "./types/config";
+
+export const siteConfig: SiteConfig = {
+	title: "尘钥ChenYue Blog",
+	description:
+		"个人博客，站长为尘钥ChenYue",
+
+	keywords: [],
+	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+	themeColor: {
+		hue: 361, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: true, // Hide the theme color picker for visitors
+		forceDarkMode: true, // Force dark mode and hide theme switcher
+	},
+	banner: {
+		enable: false,
+		src: "/xinghui.avif", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+
+		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		credit: {
+			enable: true, // Display the credit text of the banner image
+			text: "Pixiv @chokei", // Credit text to be displayed
+
+			url: "https://www.pixiv.net/artworks/122782209", // (Optional) URL link to the original artwork or artist's page
+		},
+	},
+	background: {
+		enable: true, // Enable background image
+		src: "https://eo-pic.blog.chenyue.art:958/h", // Background image URL (supports HTTPS)
+		position: "center", // Background position: 'top', 'center', 'bottom'
+		size: "cover", // Background size: 'cover', 'contain', 'auto'
+		repeat: "no-repeat", // Background repeat: 'no-repeat', 'repeat', 'repeat-x', 'repeat-y'
+		attachment: "fixed", // Background attachment: 'fixed', 'scroll', 'local'
+		opacity: 0.5, // Background opacity (0-1)
+	},
+	toc: {
+		enable: true, // Display the table of contents on the right side of the post
+		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+	},
+	favicon: [
+		// Leave this array empty to use the default favicon
+		{
+			src: "/favicon/cyico.png", // Path of the favicon, relative to the /public directory
+			//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+			//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		},
+	],
+};
+
+export const navBarConfig: NavBarConfig = {
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		LinkPreset.About,
+		{
+			name: "友链",
+			url: "/friends/", // Internal links should not include the base path, as it is automatically added
+			external: false, // Show an external link icon and will open in a new tab
+		},
+		{
+			name: "赞助",
+			url: "/donate/", // Internal links should not include the base path, as it is automatically added
+			external: false, // Show an external link icon and will open in a new tab
+		},
+	],
+};
+
+export const profileConfig: ProfileConfig = {
+	avatar: "https://q2.qlogo.cn/headimg_dl?dst_uin=2726730791&spec=5", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "尘钥ChenYue",
+	bio: "🏫一名在读高三生🏫",
+	links: [
+		{
+			name: "Bilibli",
+			icon: "fa6-brands:bilibili",
+			url: "https://space.bilibili.com/512180655",
+		},
+		{
+			name: "GitHub",
+			icon: "fa6-brands:github",
+			url: "https://github.com/ChenYue957",
+		},
+	],
+};
+
+export const licenseConfig: LicenseConfig = {
+	enable: true,
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
+
+export const imageFallbackConfig: ImageFallbackConfig = {
+	enable: true,
+	originalDomain: "eo-r2.blog.chenyue.art:958",
+	fallbackDomain: "pub-d433ca7edaa74994b3d7c40a7fd7d9ac.r2.dev",
+};
+
+export const umamiConfig: UmamiConfig = {
+	enable: true,
+	baseUrl: "https://umami.blog.chenyue.art:958",
+	shareId: "ZyDjOrmjaBTlmGtd",
+	timezone: "Asia/Shanghai",
+};
---
+++ src/content/spec/about.md
@@ -0,0 +1,18 @@
+# 关于
+
+# 
+
+**Fuwari牛逼，我这辈子都不会改博客框架了**
+
+::github{repo="saicaca/fuwari"}
+
+---
+
+# 2025/08/06
+
+新增域名chenyue.top(原为chenyue.fun)
+
+# 2025/12/28
+新增域名chenyue.art
+旧域名chenyue.fun报废
+
